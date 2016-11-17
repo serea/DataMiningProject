5395d936347b17e249440a7a84be114d35819189
wen zan <wen zan>
2016-11-17 20:16:47 +0800

    改用交叉验证法

--- zanwen/knn.py
+++ zanwen/knn.py
@@ -27,19 +27,33 @@ def preproces(rawDataPath, cleanDataPath):
     with open(cleanDataPath, 'w') as f:
         f.write(outstream)
 
-# 把预处理过的数据按9:1的比例随机分为训练集和测试集
-def divDataSet(cleanDataPath, trainDataPath, testDataPath):
-    with open(cleanDataPath,'r') as f:
-        lines = f.readlines()
+# 交叉验证法，第i份化入测试集，其他九分化为训练集
+def divDataSet(cleanDataPath, i):
+    trainData = {"group":[],"labels":[]}
+    testData = {"group":[],"labels":[]}
+    group,labels = createDataSet(cleanDataPath)
+    # 随机划分
+    dataSetIndicies = range(len(group))
+    random.shuffle(dataSetIndicies)
 
-    random.shuffle(lines)
-    trainData = lines[len(lines)/10:]
-    testData = lines[:len(lines)/10]
+    if i >= 0 and i < 10:
+        trainDataIndex = dataSetIndicies[:len(group) / 10 * i]
+        trainDataIndex.extend(dataSetIndicies[len(group) / 10 * (i+1):])
+        testDataIndex = dataSetIndicies[len(group) / 10 * i:len(group) / 10 * (i+1)]
 
-    with open(trainDataPath,'w') as f:
-        f.writelines(trainData)
-    with open(testDataPath,'w') as f:
-        f.writelines(testData)
+        for index in trainDataIndex:
+            trainData["group"].append(group[index])
+            trainData["labels"].append(labels[index])
+        trainData["group"] = array(trainData["group"])
+
+        for index in testDataIndex:
+            testData["group"].append(group[index])
+            testData["labels"].append(labels[index])
+        testData["group"] = array(testData["group"])
+    else:
+        print "Num should be a int in 0..9"
+
+    return trainData,testData
 
 def createDataSet(dataPath):
     group = []
@@ -66,17 +80,23 @@ def getLpDistances(lp, inX, dataSet):
     return distances
 
 def classify(inX, dataSet, labels, k, lp):
-    sortedDistIndicies = getLpDistances(lp,inX,dataSet).argsort()
+    Distances = getLpDistances(lp,inX,dataSet)
+    sortedDistIndicies = Distances.argsort()
     classCount = {}
+
     for i in range(k):
         voteLabel = labels[sortedDistIndicies[i]]
         classCount[voteLabel] = classCount.get(voteLabel,0) + 1
     sortedClassCount = sorted(classCount.iteritems(),key = operator.itemgetter(1), reverse = True)
     return sortedClassCount[0][0]
 
-def dataClassTest(trainDataPath, testDataPath, k, lp,testNum):
-    trainSet, trainLabels = createDataSet(trainDataPath)
-    testSet, testLabels = createDataSet(testDataPath)
+def dataClassTest(cleanDataPath, k, lp,i):
+    trainData, testData = divDataSet(cleanDataPath, i)
+
+    trainSet = trainData["group"]
+    trainLabels = trainData["labels"]
+    testSet = testData["group"]
+    testLabels = testData["labels"]
 
     testSetSize = testSet.shape[0]
     errorCount = 0
@@ -87,24 +107,23 @@ def dataClassTest(trainDataPath, testDataPath, k, lp,testNum):
         if classifierResult == testLabels[testCount]:
             pass
             # print "[Correct Classification] the classifier came back with : %s, the real answer is: %s"\
-            #       % (classifierResult,testLabels[testNum])
+            #       % (classifierResult,testLabels[testCount])
         else:
             errorCount += 1
             # print "[Wrong Classification] the classifier came back with : %s, the real answer is: %s" \
-            #       % (classifierResult, testLabels[testNum])
+            #       % (classifierResult, testLabels[testCount])
         testCount += 1
-    print "[testNum: %d, k: %d, lp: %d] The error rate is :%.3f%%" %(testNum,k,lp,errorCount/float(testSetSize)*100)
+    # print "[testNum: %d, k: %d, lp: %d] The error rate is :%.3f%%" %(testNum,k,lp,errorCount/float(testSetSize)*100)
     return errorCount/float(testSetSize)
 
-def findBestArgs(cleanDataPath,trainDataPath,testDataPath, testNum, maxK, maxLp):
+def findBestArgs(cleanDataPath, maxK, maxLp):
     minRecord = {"k":None,"lp":None,"minErrorRate":1}   # 记录最小错误率以及对应的k,lp
     for k in range(1,maxK+1):
         for lp in range(1,maxLp+1):
             totalRate = 0.0
-            for testNum in range(testNum):
-                divDataSet(cleanDataPath, trainDataPath, testDataPath)
-                totalRate += dataClassTest(trainDataPath, testDataPath, k, lp,testNum)
-            totalRate /= testNum
+            for i in range(10):
+                totalRate += dataClassTest(cleanDataPath, k, lp,i)
+            totalRate /= 10
             print("totalRate: %f%%, k: %d, lp: %d" %(totalRate*100,k,lp))
             if totalRate < minRecord["minErrorRate"]:
                 minRecord['k'] = k;
@@ -116,11 +135,9 @@ def findBestArgs(cleanDataPath,trainDataPath,testDataPath, testNum, maxK, maxLp)
 if __name__ == "__main__":
     rawDataPath = unicode("C:/Users/Mr.x/repos/DataMiningProject/zanwen/data/rawdata.csv",'utf-8')
     cleanDataPath = unicode("C:/Users/Mr.x/repos/DataMiningProject/zanwen/data/cleandata.csv",'utf-8')
-    trainDataPath = unicode("C:/Users/Mr.x/repos/DataMiningProject/zanwen/data/traindata.csv",'utf-8')
-    testDataPath = unicode("C:/Users/Mr.x/repos/DataMiningProject/zanwen/data/testdata.csv",'utf-8')
 
-    testNum=1000
-    maxK= 1
-    maxLp= 3
-    minRecord = findBestArgs(cleanDataPath,trainDataPath,testDataPath, testNum, maxK, maxLp)
+    testNum=100
+    maxK= 5
+    maxLp= 5
+    minRecord = findBestArgs(cleanDataPath, maxK, maxLp)
     print "Minimal error rate: %f%%, when k: %d, lp: %d"%(minRecord['minErrorRate']*100,minRecord['k'], minRecord['lp'])
